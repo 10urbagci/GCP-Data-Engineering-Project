431438a6e35538503c8f14ed2d72175c72197942
10urbagci <onurbagci2058@gmail.com>
2023-10-21 11:38:40 +0300

    Project updated

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-    credentials = file("peerless-tiger-400109-d76af527198c.json")
+    credentials = file("credential-file.json")
     project = var.google_project_id
     region = "us-central1"
   
